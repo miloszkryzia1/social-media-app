36ab9fbf43b0cbdb51c03f494d75509b8ddeca96
miloszkryzia1 <milosz.kryzia@gmail.com>
2024-07-05 20:25:29 -0700

    django db setup

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "social_media_db",
+        "HOST": "localhost",
+        "USER": "postgres",
+        "PASSWORD": "udemy"
     }
 }
 
