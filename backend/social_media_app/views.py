52f70bdf10bf5dd3f4493a821c9a50add67ee0ba
miloszkryzia1 <milosz.kryzia@gmail.com>
2024-07-05 20:49:08 -0700

    list endpoints needed

--- backend/social_media_app/views.py
+++ backend/social_media_app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+"""
+List of endpoints needed:
+
+Friends:
+- send friend request
+- accept friend request
+- decline friend request
+- remove friend
+- get all friends of a user
+- get all friend requests sent to a user
+- get friendship status between 2 users (friends/notfriends/request sent)
+
+Posts:
+- create a post
+- remove a post
+- edit a post
+- comment on a post
+- remove comment
+- like a post
+- remove like
+"""
