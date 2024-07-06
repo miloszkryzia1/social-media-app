ae1e63bd99941ca302a6c0191b158f9dabba1dad
miloszkryzia1 <milosz.kryzia@gmail.com>
2024-07-05 20:39:47 -0700

    django setup app and models

---
+++ backend/social_media_app/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+class Account(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    date_of_birth = models.DateField()
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=100)
+    friend = models.ManyToManyField("self", db_table="friendship")
+
+    class Meta:
+        db_table = "account"
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Account, on_delete=models.CASCADE, null=False)
+    text = models.CharField(max_length=1000)
+    image = models.ImageField()
+
+    class Meta:
+        db_table = "post"
+
+class Like(models.Model):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "like"
+
+class Comment(models.Model):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.CharField(max_length=300)
+
+    class Meta:
+        db_table = "comment"
